09077703f517e0a5bc854cdaf1640bacbb4ed1bb
TommasoUrsomando <115471825+TommasoUrsomando@users.noreply.github.com>
2022-12-05 17:33:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,2 +1,15 @@
 
-bool funzione (char &a);
+#include "lib.h"
+bool funzione (char &a){
+        if ((a >= 65) and (a <= 90)) {
+            a = (a + 32);
+            return true;
+        }
+        else if ((a >= 97) and (a <= 122)) {
+            a = (a - 32);
+            return true;
+        }
+        else{
+        return false;
+    }
+}
